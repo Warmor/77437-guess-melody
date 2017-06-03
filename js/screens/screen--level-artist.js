6420d18d5586134a022e736a7dc26197a608ee6b
Warmor <grishin.pd@gmail.com>
2017-06-03 12:53:41 +0300

    Исправление верстки
    вынес <div> из <svg>

--- js/screens/screen--level-artist.js
+++ js/screens/screen--level-artist.js
@@ -5,17 +5,14 @@ import screenLevelGenre from './screen--level-genre';
 const template = `
 <section class="main main--level main--level-artist">
   <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
-    <circle
-      cx="390" cy="390" r="370"
-      class="timer-line"
-      style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
-
-    <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
-      <span class="timer-value-mins">02</span><!--
-      --><span class="timer-value-dots">:</span><!--
-      --><span class="timer-value-secs">00</span>
-    </div>
+    <circle cx="390" cy="390" r="370" class="timer-line" style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
   </svg>
+
+  <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
+    <span class="timer-value-mins">02</span><!--
+    --><span class="timer-value-dots">:</span><!--
+    --><span class="timer-value-secs">00</span>
+  </div>
 
   <div class="main-wrap">
     <div class="main-timer"></div>
