b65f99ca19658c740c908b5eeed18a9b053d294e
Warmor <grishin.pd@gmail.com>
2017-06-25 04:34:24 +0300

    вернул тесты

--- js/data/statistics-data.js
+++ js/data/statistics-data.js
@@ -20,7 +20,7 @@ class StatisticsData {
     newStatistick.sort(function (a, b) {
       return b.score - a.score || a.time - b.time;
     });
-    return Math.trunc(((newStatistick.length - (newStatistick.indexOf(myStatistick))) / newStatistick.length) * 100);
+    return Math.trunc(((newStatistick.length - (newStatistick.indexOf(myStatistick) + 1)) / newStatistick.length) * 100);
   }
 
 }
