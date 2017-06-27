6fec64fc46328d86000e4c758f77ed828cc472b8
Warmor <grishin.pd@gmail.com>
2017-06-27 18:29:01 +0300

    Добавил прелоадер

---
+++ js/preloader/preloader-view.js
@@ -0,0 +1,17 @@
+import View from '../view';
+
+export default class PreloaderView extends View {
+  constructor() {
+    super();
+  }
+
+  get template() {
+    return `
+      <div class="cssload-loader">
+        <div class="cssload-inner cssload-one"></div>
+        <div class="cssload-inner cssload-two"></div>
+        <div class="cssload-inner cssload-three"></div>
+      </div>`;
+  }
+
+}
