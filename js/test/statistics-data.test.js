2fdf96f4266d7b80bf73a53fd4174418c3389e33
Warmor <grishin.pd@gmail.com>
2017-06-28 17:40:24 +0300

    удалил тесты, модуль ссо статистикой больше не тестируемый

--- js/test/statistics-data.test.js
+++ js/test/statistics-data.test.js
@@ -1,19 +1,9 @@
-import statisticsData from '../data/statistics-data';
-
 import assert from 'assert';
 
-// describe(`getPercentage`, () => {
+describe(`fake test`, () => {
 
-//   it(`percentage should return 90% for 1st`, () => {
-//     assert.equal(90, statisticsData.getPercentage(10, 10));
-//   });
+  it(`1 === 1`, () => {
+    assert.equal(1, 1);
+  });
 
-//   it(`percentage should return 50% for 5st`, () => {
-//     assert.equal(50, statisticsData.getPercentage(10, 6));
-//   });
-
-//   it(`percentage should return 0% for last st`, () => {
-//     assert.equal(0, statisticsData.getPercentage(10, 1));
-//   });
-
-// });
+});
